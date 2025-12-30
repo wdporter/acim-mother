62229f7e24ebbff1539dacdd57916264f89b80b2
W. David Porter <david.porter@fastmail.com>
2025-12-30 12:58:39 +1000

    change dev port

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,6 +1,10 @@
 export default async function(eleventyConfig) {
 	// Configure Eleventy
 	eleventyConfig.setInputDirectory("src")
+	eleventyConfig.setServerOptions({
+		port: 8090
+	})
 	eleventyConfig.addPassthroughCopy("src/bundle.css")
+
 
 };
