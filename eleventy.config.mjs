c24b2d675d69c1ab0fba646fdf2dd431af4ece92
W. David Porter <david.porter@fastmail.com>
2026-01-08 20:57:01 +1000

    use tailwind

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,6 +1,12 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import markdownItAttrs  from "markdown-it-attrs";
 
+import fs from 'fs';
+import path from 'path';
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
+
+
 export default async function(eleventyConfig) {
 	// Configure Eleventy
 	eleventyConfig.setInputDirectory("src")
@@ -9,7 +15,36 @@ export default async function(eleventyConfig) {
 	})
 	eleventyConfig.addPlugin(eleventyNavigationPlugin)
 	eleventyConfig.addPassthroughCopy("src/assets")
+	eleventyConfig.addPassthroughCopy({"_temp/tailwind.css": "assets/tailwind.css"})
 	
 	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItAttrs));
 
+	//compile tailwind before eleventy processes the files
+  eleventyConfig.on('eleventy.before', async () => {
+    const tailwindInputPath = path.resolve('./src/tailwind/tailwind.css');
+
+    const tailwindOutputPath = './_temp/tailwind.css';
+
+    const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+
+    const outputDir = path.dirname(tailwindOutputPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await processor.process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  const processor = postcss([
+    //compile tailwind
+    tailwindcss(),
+  ]);
+	
+	
+	
 };
